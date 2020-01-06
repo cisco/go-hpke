c8ff22bfe465ff29d3c78a81841e1c7a7ca4d580
Richard Barnes <rlb@ipv.sx>
2020-01-06 13:32:07 -0500

    Update for better spec compliance
    * Correct KEM ID numbers
    * Increment sequence number *after* seal/open

--- hpke.go
+++ hpke.go
@@ -213,12 +213,7 @@ func newCipherContext(suite CipherSuite, setupParams setupParameters, contextPar
 	return cipherContext{key, nonce, aead, 0, nil, setupParams, contextParams}, nil
 }
 
-func (ctx *cipherContext) makeNonce() []byte {
-	ctx.seq += 1
-	if ctx.seq == 0 {
-		panic("sequence number wrapped")
-	}
-
+func (ctx *cipherContext) currNonce() []byte {
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, ctx.seq)
 
@@ -231,6 +226,13 @@ func (ctx *cipherContext) makeNonce() []byte {
 
 	ctx.nonces = append(ctx.nonces, nonce)
 	return nonce
+}
+
+func (ctx *cipherContext) incrementSeq() {
+	ctx.seq += 1
+	if ctx.seq == 0 {
+		panic("sequence number wrapped")
+	}
 }
 
 type EncryptContext struct {
@@ -247,7 +249,8 @@ func newEncryptContext(suite CipherSuite, setupParams setupParameters, contextPa
 }
 
 func (ctx *EncryptContext) Seal(aad, pt []byte) []byte {
-	ct := ctx.aead.Seal(nil, ctx.makeNonce(), pt, aad)
+	ct := ctx.aead.Seal(nil, ctx.currNonce(), pt, aad)
+	ctx.incrementSeq()
 	return ct
 }
 
@@ -265,11 +268,12 @@ func newDecryptContext(suite CipherSuite, setupParams setupParameters, contextPa
 }
 
 func (ctx *DecryptContext) Open(aad, ct []byte) ([]byte, error) {
-	pt, err := ctx.aead.Open(nil, ctx.makeNonce(), ct, aad)
+	pt, err := ctx.aead.Open(nil, ctx.currNonce(), ct, aad)
 	if err != nil {
 		return nil, err
 	}
 
+	ctx.incrementSeq()
 	return pt, nil
 }
 
